29345e128b937c66cd058b6cc6c4ba1b73136cc9
Satyamurthi <87005236+Satyamurthi@users.noreply.github.com>
2021-12-04 17:37:14 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -1,89 +1,36 @@
-import datetime
+import pymongo 
+import os
 
-import motor.motor_asyncio
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["user"]
 
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"file_id":None}
+            try:
+            	dbcol.insert_one(user_det)
+            except:
+            	pass
 
-class Database:
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
+def addthumb(chat_id, file_id):
+	dbcol.update_one({"_id":chat_id},{"$set":{"file_id":file_id}})
+	
+def delthumb(chat_id):
+	dbcol.update_one({"_id":chat_id},{"$set":{"file_id":None}})
+	
+def find(chat_id):
+	id =  {"_id":chat_id}
+	x = dbcol.find(id)
+	for i in x:
+             lgcd = i["file_id"]
+             return lgcd 
 
-    def new_user(self, id):
-        return dict(
-            id=id,
-            join_date=datetime.date.today().isoformat(),
-            notif=True,
-            ban_status=dict(
-                is_banned=False,
-                ban_duration=0,
-                banned_on=datetime.date.max.isoformat(),
-                ban_reason="",
-            ),
-        )
-
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
-
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({"id": int(id)})
-        return True if user else False
-
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
-
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
-
-    async def delete_user(self, user_id):
-        await self.col.delete_many({"id": int(user_id)})
-
-    async def remove_ban(self, id):
-        ban_status = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
-        )
-        await self.col.update_one({"id": id}, {"$set": {"ban_status": ban_status}})
-
-    async def ban_user(self, user_id, ban_duration, ban_reason):
-        ban_status = dict(
-            is_banned=True,
-            ban_duration=ban_duration,
-            banned_on=datetime.date.today().isoformat(),
-            ban_reason=ban_reason,
-        )
-        await self.col.update_one({"id": user_id}, {"$set": {"ban_status": ban_status}})
-
-    async def get_ban_status(self, id):
-        default = dict(
-            is_banned=False,
-            ban_duration=0,
-            banned_on=datetime.date.max.isoformat(),
-            ban_reason="",
-        )
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("ban_status", default)
-
-    async def get_all_banned_users(self):
-        banned_users = self.col.find({"ban_status.is_banned": True})
-        return banned_users
-
-    async def set_notif(self, id, notif):
-        await self.col.update_one({"id": id}, {"$set": {"notif": notif}})
-
-    async def get_notif(self, id):
-        user = await self.col.find_one({"id": int(id)})
-        return user.get("notif", False)
-
-    async def get_all_notif_user(self):
-        notif_users = self.col.find({"notif": True})
-        return notif_users
-
-    async def total_notif_users_count(self):
-        count = await self.col.count_documents({"notif": True})
-        return count
+def getid():
+    values = []
+    for key  in dbcol.find():
+         id = key["_id"]
+         values.append((id)) 
+    return values
